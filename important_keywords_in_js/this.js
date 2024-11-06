8fabefcb0a56a8fde7103530c970ec85b1e6546e
prahulk46a <prahulk212@gmail.com>
2024-11-06 09:55:34 +0530

    Freeze,seal in js also this keyword in js

---
+++ important_keywords_in_js/this.js
@@ -0,0 +1,57 @@
+const person={
+    name:"Rahul",
+    //function should be simple only no arrow function should be used as it will points towards window
+    greet:function(){
+        console.log(this); //this will point to current obj 
+    }
+}
+// person.greet();
+
+// console.log(this);
+
+let obj={
+    expression:"😭",
+    f1:function(){
+        //only this synario will point towards current obj using arrow because of lexical scoping(ability to search in global)
+        let f2=()=>{
+            console.log(this);
+        }
+    }
+}
+
+//
+let obj2={
+    name:"Rahul",
+    age:24
+};
+function f1(){
+    console.log(this);
+}
+
+// f1(); //this will point towards window obj but we want that this inside funct should point to current obj2 
+
+// f1.call(obj2); //by doing this it will point towards current obj.
+
+
+
+//Ways to pointing current obj to this=> call bind apply  and diffrence
+
+let obj3={
+    name:"Akanksha",
+    age:24
+};
+function f2(a,b,c){
+    console.log(this);
+    console.log(a,b,c);
+}
+
+f2.call(obj3,10,20,30);
+
+f2.apply(obj3,[10,20,30]);   //just syntax diffrence here in 3 methods
+
+let returnedFunc=f2.bind(obj3,10,20,30); //will return refrence of this current funct and can be called in future
+returnedFunc()
+
+
+
+
